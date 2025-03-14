80374502e2201f394ee3f6db5664b1363c3be1dc
SAX Technology <SaxIT@saxllp.com>
2025-03-14 19:32:40 -0400

    Update run.ps1

--- Ms365-GetGroups/run.ps1
+++ Ms365-GetGroups/run.ps1
@@ -1,3 +1,13 @@
+using namespace System.Net
+
+param($Request, $TriggerMetadata)
+
+Write-Host "Get Group function triggered."
+
+$resultCode = 200
+$message = ""
+
+
     #Connect to Graph
     $secure365Password = ConvertTo-SecureString -String $env:Ms365_AuthSecretId -AsPlainText -Force
     $credential365 = New-Object System.Management.Automation.PSCredential($env:Ms365_AuthAppId, $secure365Password)

218486ed6e48fe4f17c826f099ecec0a77bda170
SAX Technology <SaxIT@saxllp.com>
2025-03-14 19:32:01 -0400

    Update run.ps1

--- Ms365-GetGroups/run.ps1
+++ Ms365-GetGroups/run.ps1
@@ -1,1 +1,6 @@
+    #Connect to Graph
+    $secure365Password = ConvertTo-SecureString -String $env:Ms365_AuthSecretId -AsPlainText -Force
+    $credential365 = New-Object System.Management.Automation.PSCredential($env:Ms365_AuthAppId, $secure365Password)
 
+    Connect-MgGraph -ClientSecretCredential $credential365 -TenantId $TenantId
+    get-mggroup -sort displayName
